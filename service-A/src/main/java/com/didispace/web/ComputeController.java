2efbf9e3cf32908f80db92195090db4c7e310be6
yangfan <shiki0@qq.com>
2017-11-01 15:31:55 +0800

    配置格式改yml

--- service-A/src/main/java/com/didispace/web/ComputeController.java
+++ service-A/src/main/java/com/didispace/web/ComputeController.java
@@ -22,7 +22,7 @@ public class ComputeController {
         ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a + b;
         logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
-        return "From Service-A, Result is " + r;
+        return "From Service-B, Result is " + r;
     }
 
 }
